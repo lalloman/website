04594810dcc2bd96debe363a1858d7811aa80562
lalloman <205902@gardenschool.edu.my>
2022-10-31 18:51:11 +0800

    inital upload

---
+++ Starter/assets/js/main.js
@@ -0,0 +1,18 @@
+// Grab elements
+const selectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+};
+
+//Nav styles on scroll
+
+// Open menu & search pop-up
+
+// Open/Close search form popup
+
+// -- Close the search form popup on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
